200401b31aec27259ccee62c42f177a1eb3cc72d
yiğitcan Öztürk <yozturk037@gmail.com>
2021-09-03 10:17:39 +0300

    yandex api

---
+++ src/components/TranlateForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="translateIt" class="well">
+      <textarea
+        v-model="translateText"
+        cols="30"
+        rows="5"
+        class="form-control"
+        placeholder="Çevirmek istediğiniz kelime/cümle yazınız."
+      ></textarea>
+      <select v-model="translateTo" class="form-control">
+        <option>1</option>
+      </select>
+      <br />
+      <div class="text-left">
+        <strong>Tespit Edilen Dil : </strong>
+      </div>
+      <br />
+      <button type="submit" class="btn btn-primary btn-block">
+        Çevir Gelsin!
+      </button>
+    </form>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      translateText: '',
+      translateTo: '',
+    }
+  },
+  methods: {
+    translateIt() {
+      alert(this.translateTo);
+      alert(this.translateText);
+    }
+  }
+}
+</script>
+<style>
+</style>
